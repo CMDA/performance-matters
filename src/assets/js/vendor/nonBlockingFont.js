b314702ceaec22725a17e00e4b837ba0950c3228
ColinDorr <colin.maloud@gmail.com>
2017-03-14 20:53:19 +0100

    non blocking font script

--- src/assets/js/vendor/nonBlockingFont.js
+++ src/assets/js/vendor/nonBlockingFont.js
@@ -1,8 +1,8 @@
 function createCookie(name, value) {
         document.cookie = `${name}=${value}; path=/`;
       }
-      const font = new FontFaceObserver('source_sans_pro');
-      font.load().then(function () {
+      var font = new FontFaceObserver('source_sans_pro');
+      font.load(null, 20000).then(function () {
         document.documentElement.className += ' fonts-loaded';
         createCookie('fontsLoaded', true);
       });
