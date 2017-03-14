773ecc40fd56c2aa3f9ae7b15f6414c89ae57d3b
ColinDorr <colin.maloud@gmail.com>
2017-03-14 21:10:58 +0100

    longer load font time detection

--- src/assets/js/vendor/nonBlockingFont.js
+++ src/assets/js/vendor/nonBlockingFont.js
@@ -3,7 +3,7 @@
     }
     var html = document.documentElement;
     var font = new FontFaceObserver('source_sans_pro');
-    font.load(null, 15000).then(function () {
+    font.load(null, 30000).then(function () {
         document.documentElement.className += ' fonts-loaded';
         createCookie('fontsLoaded', true);
     });
