31a7b901ed63344cdef5e7d387867d06da9b4609
ColinDorr <colin.maloud@gmail.com>
2017-03-15 12:40:03 +0100

    added gulp critical css

--- src/assets/js/gulpScripts.js
+++ src/assets/js/gulpScripts.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp');
 const imagemin = require('gulp-imagemin');
-
+const cleanCSS = require('gulp-clean-css');
+const critical = require('critical');
 
 //Images compression
 gulp.task('default', () =>
@@ -8,3 +9,25 @@ gulp.task('default', () =>
         .pipe(imagemin())
         .pipe(gulp.dest('src/assets/img'))
 );
+
+//Critical CSS
+gulp.task('minify-css', function() {
+  return gulp.src('styles/*.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('cssCritical', () => {
+	const htmlPage = fs.readFileSync('./src/index.html');
+	critical.generate({
+		inline: false,
+		base: './',
+		html: htmlPage,
+    minify: true,
+		src: './src/_base/layout.html',
+		css: ['https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css', './src/dist/css/docs.css'],
+		dest: './src/dist/css/critical.css',
+		width: 1920,
+		height: 1080
+	});
+});
