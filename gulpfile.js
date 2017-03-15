b8d2b6030614be8152b886c0ae84e74e1e71a1ae
Sjoerdbeentjes <mail@sjoerdbeentjes.nl>
2017-03-15 12:35:17 +0100

    installed & implemented gulp-webp

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,8 @@
 const gulp = require('gulp');
+const webp = require('gulp-webp');
+
+gulp.task('webp', () =>
+    gulp.src('src/assets/img/*')
+        .pipe(webp())
+        .pipe(gulp.dest('src/dist/img/'))
+);
